80b814565fa985c948798aca7a1b827d953c76d4
Carlxu <carl.zheheng@gmail.com>
2018-02-08 14:28:32 +0800

    first commit

--- js/com.js
+++ js/com.js
@@ -1,3 +1,9 @@
-$(function(){
-    
-});
+//滚轮兼容事件
+function addWheelEvent(obj, eFn) {
+    document.addEventListener ? obj.addEventListener(document.createElement("div").onmousewheel === null ?
+        "mousewheel" : "DOMMouseScroll", fn, false) : obj.attachEvent("onmousewheel", fn);
+
+    function fn(e) {
+        eFn.call(obj, e = e || window.event, e.wheelDelta / 120 || -e.detail / 3);
+    }
+}
